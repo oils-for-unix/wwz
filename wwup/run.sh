d0ecc374eeb2a0a669b74ab98e9d29ee8b6bc92e
travis_admin <travis_admin@fluvanna.dreamhost.com>
2024-05-03 14:08:01 -0700

    [wwup] Basic skeleton working
    Testing it locally

--- wwup/run.sh
+++ wwup/run.sh
@@ -7,14 +7,47 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
-readonly ROOT=~/travis-ci.oilshell.org
+readonly HOST=travis-ci.oilshell.org
+readonly DIR=travis-ci.oilshell.org
 
 setup() {
-  mkdir -v -p $ROOT/untrusted	  
+  # /U/ for for 'untrusted user uploads'
+  ssh $HOST "mkdir -v -p $DIR/U"
 }
 
+readonly URL=http://travis-ci.oilshell.org/wwup.cgi
+
 upload-one() {
-  curl --form 'file=@README.md' http://travis-ci.oilshell.org/wwup.cgi
+  curl \
+    --form 'payload-type=osh-runtime' \
+    --form 'zip=@_tmp/one.zip' \
+    $URL
+}
+
+upload-bad-type() {
+  # missing
+  curl \
+    --form 'zip=@_tmp/one.zip' \
+    http://travis-ci.oilshell.org/wwup.cgi
+
+  # bad
+  curl \
+    --form 'payload-type=zzz' \
+    --form 'zip=@_tmp/one.zip' \
+    http://travis-ci.oilshell.org/wwup.cgi
+}
+
+upload-bad-zip() {
+  # missing
+  curl \
+    --form 'payload-type=osh-runtime' \
+    http://travis-ci.oilshell.org/wwup.cgi
+
+  # bad
+  curl \
+    --form 'payload-type=osh-runtime' \
+    --form 'zip=@README.md' \
+    http://travis-ci.oilshell.org/wwup.cgi
 }
 
 upload-many() {
@@ -27,9 +60,46 @@ upload-many() {
     http://travis-ci.oilshell.org/wwup.cgi
 }
 
+make-zips() {
+  mkdir -p _tmp/{osh-runtime,shell-id,host-id,bad}
+
+  for file in \
+    _tmp/osh-runtime/aa.tsv \
+    _tmp/shell-id/bb.txt \
+    _tmp/host-id/cc.txt \
+    _tmp/host-id/bad.js \
+    _tmp/bad/zz.txt;
+  do
+     echo x > $file
+  done
+
+  rm -f -v _tmp/one.zip
+
+  zip _tmp/one.zip _tmp/{osh-runtime,shell-id,host-id,bad}/*
+
+  unzip -l _tmp/one.zip
+}
+
+local-test() {
+  # I guess I should pass upload dir
+  ./wwup.cgi
+}
+
 demo() {
-  cp -v wwup.cgi $ROOT
-  upload-many
+  scp wwup.cgi $HOST:$DIR
+
+  set +o errexit
+  upload-bad-type
+  echo status=$?
+  echo
+
+  upload-bad-zip
+  echo status=$?
+  echo
+
+  upload-one
+  echo status=$?
+  echo
 }
 
 "$@"
--- wwup/wwup.cgi
+++ wwup/wwup.cgi
@@ -7,29 +7,68 @@ import cgi
 # files with .wwz extension are not extracted?
 import zipfile
 
+
+# Hard-coded payload validation:
+
+# definitely no .js .css .html
+ALLOWED_EXTENSIONS = ['.txt', '.tsv', '.csv', '.json']
+
+# Additional rule: you can never overwrite a file!  You can only create new
+# files.
+
+
+# Rules by payload:
+
+PAYLOAD_GLOBS = {
+  'osh-runtime': {
+    'globs': [
+      'osh-runtime/*.tsv',  # times, provenance, gc-stats
+      'shell-id/*-*/*.txt',
+      'host-id/*-*/*.txt',
+    ],
+    'max_files': 20,
+    'max_bytes': 10 * 1000 * 1000,  # 10 MB
+  }
+}
+
+
 #cgi.test()
 
-def main():
+def Upload():
   form = cgi.FieldStorage()
+
   print('Content-Type: text/plain; charset=utf-8')
   print('')
+  print('OK')
 
-  print(form)
+  # Form fields
+  # - payload-type=osh-runtime
+  # - zip=foo.zip  # files to extract
 
-  print()
-  print(form['files[]'])
-  print()
-  print(form['files2'])
-  print()
+  payload_type = form.getfirst('payload-type')
+  if payload_type is None:
+      raise RuntimeError('Expected payload type')
+  if payload_type not in PAYLOAD_GLOBS:
+      raise RuntimeError('Invalid payload type %r' % payload_type)
+
+  zip_contents = form.getfirst('zip')
+  if zip_contents is None:
+      raise RuntimeError('Expected zip')
+
+  print('t %r' % t)
+  print('z %r' % z)
 
 
-  from pprint import pprint
+def main():
 
-  print('LIST')
-  print()
-  pprint(form.getlist('files[]'), indent=4)
-  print()
-  pprint(form.getlist('files2'), indent=4)
+  try:
+    Upload()
+  except RuntimeError as e:
+    # CGI has a Status: header!
+    print('Status: 400 Bad Request')
+    print('Content-Type: text/plain; charset=utf-8')
+    print('')
+    print('Bad request: %s' % e)
 
 
 main()
